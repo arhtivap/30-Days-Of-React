68887da25abc09ff38b6b417884bf95c6e1863a1
Pavithra Sunkesulu <pavi.kec@gmail.com>
2023-10-10 12:02:39 -0300

    Refreshing JavaScript.

---
+++ Day-1-JavaScript-Refresher/app.js
@@ -0,0 +1,45 @@
+//This is a JavaScript refresher. 
+
+//Arrays with initial values and using length property to find the length of an array.
+
+const numbers = [0, 3.14, 9.81, 37, 98.6, 100]; //Array of numbers.
+
+const fruits = ['Banana', 'Orange', 'Mango', 'Lemon']; //Array of strings, fruits.
+
+const vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot']; //Array of strings, vegetables.
+
+const animalProducts = ['Milk', 'Meat', 'Butter', 'Yogurt']; //Array of strings, products.
+
+const webTechs = ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'MongoDB']; //Array of strings, web technologies.
+
+const countries = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland']; //Array of strings, countries.
+
+//Printing the arrays and its length.
+
+console.log('Numbers:', numbers);
+console.log('Numbers in Length:', numbers.length);
+
+console.log('Fruits:', fruits);
+console.log('Fruits in Length:', fruits.length);
+
+console.log('Vegetables:', vegetables);
+console.log('Vegetables in Length:', vegetables.length);
+
+console.log('Animal Products:', animalProducts);
+console.log('Animal Products in Length:', animalProducts.length);
+
+console.log('Web Technologies:', webTechs);
+console.log('Web Technologies in Length:', webTechs.length);
+
+console.log('Countries:', countries);
+console.log('Countries in Length:', countries.length);
+
+//Array with different datatypes.
+
+const arr = ['John Doe', 250, true, {country: 'Finland', city: 'Helsinki'}, {skills: ['HTML', 'CSS', 'JS', 'React', 'Python']},];
+
+//Printing the arr.
+
+console.log('Array with different datatypes: ', arr);
+
+
